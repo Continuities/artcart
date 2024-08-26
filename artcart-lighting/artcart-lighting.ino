3e7a760a60bd2827d8819fc3c4a33532a1b3fd6d
Michael Townsend <michael@doublespeakgames.com>
2024-08-26 11:49:21 -0400

    initial

---
+++ artcart-lighting/artcart-lighting.ino
@@ -0,0 +1,15 @@
+STRIP_PIN=2
+PHASE_ONE=1
+PHASE_TWO=2
+PHASE_THREE=3
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
